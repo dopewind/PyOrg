b7e4c969f52b5f104073e9f8ab4749993a1c98d2
dopewind <imkvinayan@gmail.com>
2020-09-10 13:53:54 +0530

    update folder.py

--- folders.py
+++ folders.py
@@ -3,24 +3,27 @@ from pathlib import Path
 
 DIRECTORIES = {
     "HTML": [".html5", ".html", ".htm", ".xhtml"],
-    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+    "Images": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
                ".heif", ".psd"],
-    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+    "Videos": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".mkv", ".webm", ".vob", ".mng",
                ".qt", ".mpg", ".mpeg", ".3gp"],
-    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
-                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
-                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
-                  "pptx"],
-    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
+    "Docs": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+             ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
+             ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
+             "pptx"],
+    "Archives": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
                  ".dmg", ".rar", ".xar", ".zip"],
-    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+    "Audio": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
               ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "PLAINTEXT": [".txt", ".in", ".out"],
+    "Test": [".txt", ".in", ".out"],
     "PDF": [".pdf"],
-    "PYTHON": [".py"],
+    "Python": [".py"],
     "XML": [".xml"],
-    "EXE": [".exe"],
-    "SHELL": [".sh"]
+    "Executables": [".exe"],
+    "SHELL": [".sh"],
+    "MSI": [".msi"],
+    "Registry Files": [".reg"],
+    "Android Apk": [".apk"]
 
 }
 
