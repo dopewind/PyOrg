0b4e4db1e92716fd1b9f5aa5a7609bf7e4ec79b7
dopewind <imkvinayan@gmail.com>
2020-10-17 14:25:34 +0530

    linting:2

--- dev/lint_check.py
+++ dev/lint_check.py
@@ -1,10 +1,10 @@
 import subprocess  # To run pyflakes and get output
 import glob
-from tools.change_path import c_path
+import tools.change_path as change_path
 
 
 def c_lint(dir_path):
-    c_path(dir_path)
+    change_path.c_path(dir_path)
     py_files = glob.glob('*.py', recursive=True)
     print(py_files)
     for file in py_files:
