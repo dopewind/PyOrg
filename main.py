4e9e6771add80bd72eb9eff22b5a31166911dce1
dopewind <imkvinayan@gmail.com>
2020-10-17 05:56:07 +0530

    send logs:6

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ SERVER_OWN = os.environ.get('SERVER')
 BUILD_LOG = os.environ.get('TRAVIS_JOB_WEB_URL')
 HOME_PATH = os.environ.get('HOME')
 BUILD_TYPE = os.environ.get('TRAVIS_EVENT_TYPE')
-
-BUILD_ID = BUILD_LOG[-9:]
-BUILD_LOG_URL = 'https://www.api.travis-ci.com/v3/job/' + BUILD_ID + '/log.txt'
-print(SERVER_OWN, SERVER, BUILD_ID, BUILD_LOG_URL,
-      BUILD_LOG, HOME_PATH, BUILD_TYPE)
+BUILD_BRANCH = os.environ.get('TRAVIS_BRANCH')
 
 
 # --- Constants ---
@@ -39,7 +35,7 @@ alias = {'ll': 'colorls -lA --sd',
 
 def telegram_bot_sendtext(bot_message):
 
-    bot_token = TOKEN
+    bot_token = str(TOKEN)
     bot_chatID = '1001518410'
     send_text = 'https://api.telegram.org/bot' + bot_token + \
         '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
@@ -49,12 +45,16 @@ def telegram_bot_sendtext(bot_message):
     return response.json()
 
 
-def telegram_bot_senddocs(doc_url, caption):
-    bot_token = TOKEN
+def telegram_bot_senddocs(ab_local_url):
+    bot_token = str(TOKEN)
     bot_chatID = '1001518410'
-    send_docs = 'https://api.telegram.org/bot' + bot_token + \
-        '/sendDocument?chat_id=' + bot_chatID + 'document=' + \
-        doc_url + '&parse_mode=Markdown&caption=' + caption
+    files = {
+        'chat_id': (None, bot_chatID),
+        'document': (ab_local_url, open(ab_local_url, 'rb')),
+    }
+
+    response = requests.post(
+        'https://api.telegram.org/bot' + bot_token + '/sendDocument', files=files)
 
 
 def get_logs(LOG_URL):
@@ -65,12 +65,23 @@ def get_logs(LOG_URL):
 
 # Checking if it is running on CI server
 if SERVER == 'true' or SERVER == True or SERVER_OWN == 1:
-    msg = "Travis CI just made sure this works. Hoorayy 🚀"
-    telegram_bot_sendtext(msg)
+    BUILD_ID = BUILD_LOG[-9:]
+    BUILD_LOG_URL = 'https://www.api.travis-ci.com/v3/job/' + BUILD_ID + '/log.txt'
+
+    get_logs(BUILD_LOG_URL)
+
+    print(SERVER_OWN, SERVER, BUILD_ID, BUILD_LOG_URL,
+          BUILD_LOG, HOME_PATH, BUILD_TYPE, current_time)
+
+    telegram_bot_sendtext('New build incoming')
+    telegram_bot_sendtext("Current Server Time --> " + current_time)
     telegram_bot_sendtext('Build Type --> ' + BUILD_TYPE)
     telegram_bot_sendtext('Current Build URL --> ' + BUILD_LOG)
+    telegram_bot_sendtext('Current build branch --> ' + BUILD_BRANCH)
     telegram_bot_sendtext("Sending Logs 📩")
-    telegram_bot_senddocs(BUILD_LOG_URL, current_time)
+    telegram_bot_senddocs("/home/travis/log.txt")
+    telegram_bot_sendtext("Travis CI just made sure this works. Hoorayy 🚀")
+
     exit(0)
 
 # Arguments passed to the function
@@ -102,7 +113,7 @@ elif quote == 1:
     msg = '''"''' + name + '''"''' + ' finished running'
 
 if done == 1:
-    telegram_bot_sendtext(msg)
+    telegram_bot_sendtext(current_time)
     print('Done!')
 
 
